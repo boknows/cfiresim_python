3142884592666497f0c62d0a6762761c7e0d9044
boknows <lboland@fool.com>
2017-03-15 10:29:50 -0400

    .pyc removals

--- calculator/simulation.py
+++ calculator/simulation.py
@@ -13,10 +13,24 @@ class Cycle:
 
 def run_simulation(form):
     market_data = DataPoint.objects.all().yearly()
-
     simulation_cycles = []
     if form.is_valid:
         cycle_length = form.cleaned_data['retirement_end_year'] - form.cleaned_data['retirement_year'] + 1
+        first_year_of_data = market_data.first().data_date.year
+        last_year_of_data = market_data.last().data_date.year
+        number_of_cycles = last_year_of_data - first_year_of_data - cycle_length
+
+        previous_data_points = []
+        for m in market_data:
+            if len(previous_data_points) < cycle_length - 1:
+                previous_data_points.append(m)
+                continue
+            simulation_cycles.append(Cycle(previous_data_points[0], m))
+            if len(previous_data_points) == cycle_length - 1:
+                previous_data_points.pop(0)
+            previous_data_points.append(m)
+
+        print simulation_cycles
 
     print cycle_length, len(market_data)
-    
+
